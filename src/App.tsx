4b299cedb52732fb84433012fa74e5d3075d46c3
Uros Karic <dot.kadza@gmail.com>
2020-09-07 18:41:34 +0200

    VDR

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import SocketIO from "socket.io-client";
+import ax from "axios";
+import "./App.css";
 
 function App() {
+  const [qr, setQr] = useState<any>(null);
+  const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null);
+
+  const [orderId, setOrderId] = useState<string | null>(null);
+
+  const generateQr = async (orderId: string) => {
+    const res = await ax.post(
+      "http://localhost:4000/api/v1/qr/generate",
+      {
+        orderId,
+      },
+      { responseType: "blob" }
+    );
+
+    return res;
+  };
+
+  const generateOrderId = async (): Promise<string> => {
+    const order = await ax.post("http://localhost:4000/api/v1/order/generate");
+    return order.data?.orderId;
+  };
+
+  const initateCheckout = async () => {
+    // 1. Generate Payment Request
+    // 2. Connect to WS and create room with orderId
+    // 3. Attach listeners for that orderId
+
+    try {
+      const orderId = await generateOrderId();
+
+      const qr = await generateQr(orderId);
+
+      const socket = SocketIO("http://localhost:4000", {});
+      setSocket(socket);
+
+      socket.emit("create-order", {
+        orderId,
+      });
+
+      socket.on(`ORDER_${orderId}`, (data: any) => {
+        console.log("IMAMO NESTO OD ORDERA", data);
+      });
+
+      setQr(qr.data);
+      console.log("ORDER ID", orderId);
+    } catch (e) {
+      socket?.disconnect();
+    }
+  };
+  //tekPartTP59
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <button onClick={() => initateCheckout()}>GENERATE QR</button>
+      {qr && <img src={window.URL.createObjectURL(qr)} alt="PAYMENT QR" />}
     </div>
   );
 }
